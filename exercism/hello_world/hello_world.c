e7c9aa2f7f3789e3fbac1503534f349367b559ca
henryhello <jiangchaoyouxin@qq.com>
2025-03-02 21:15:51 +0800

    update

--- exercism/hello_world/hello_world.c
+++ exercism/hello_world/hello_world.c
@@ -1,10 +1,11 @@
 #include "hello_world.h"
+
 #include <stdlib.h>
 #include <string.h>
 
 const char *hello(void)
 {
-    char *ans = strdup("Hello, World!");
+    char *ans = strdup("Hello, duandongyi World!");
     /* string is pointer of the first character */
     return ans;
 }
